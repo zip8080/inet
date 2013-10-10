cc78301b5c76c579f3161bfa799e65b2eeb42bbf
Zoltan Bojthe <zbojthe@omnetpp.org>
2013-10-10 16:08:45 +0200

    IPvXTrafSink: uses INetworkProtocolControlInfo

--- src/applications/generic/IPvXTrafSink.cc
+++ src/applications/generic/IPvXTrafSink.cc
@@ -21,8 +21,7 @@
 
 #include "AddressResolver.h"
 #include "IPSocket.h"
-#include "IPv4ControlInfo.h"
-#include "IPv6ControlInfo.h"
+#include "INetworkProtocolControlInfo.h"
 #include "ModuleAccess.h"
 #include "NodeOperations.h"
 
@@ -86,25 +85,19 @@ void IPvXTrafSink::printPacket(cPacket *msg)
     Address src, dest;
     int protocol = -1;
 
-    if (dynamic_cast<IPv4ControlInfo *>(msg->getControlInfo()) != NULL)
+    INetworkProtocolControlInfo *ctrl = dynamic_cast<INetworkProtocolControlInfo *>(msg->getControlInfo());
+
+    if (ctrl != NULL)
     {
-        IPv4ControlInfo *ctrl = (IPv4ControlInfo *)msg->getControlInfo();
-        src = ctrl->getSrcAddr();
-        dest = ctrl->getDestAddr();
-        protocol = ctrl->getProtocol();
-    }
-    else if (dynamic_cast<IPv6ControlInfo *>(msg->getControlInfo()) != NULL)
-    {
-        IPv6ControlInfo *ctrl = (IPv6ControlInfo *)msg->getControlInfo();
-        src = ctrl->getSrcAddr();
-        dest = ctrl->getDestAddr();
+        src = ctrl->getSourceAddress();
+        dest = ctrl->getDestinationAddress();
         protocol = ctrl->getProtocol();
     }
 
     EV << msg << endl;
     EV << "Payload length: " << msg->getByteLength() << " bytes" << endl;
 
-    if (protocol != -1)
+    if (ctrl != NULL)
         EV << "src: " << src << "  dest: " << dest << "  protocol=" << protocol << "\n";
 }
 
